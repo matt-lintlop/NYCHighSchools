63aea4e7df8524d531257e67f85673ea1c7487e3
Matt Lintlop <matt_lintlop@icloud.com>
2018-02-01 18:54:13 -0800

    Started to implemented Services class & HighSchoolData structure.

---
+++ NYCHighSchools/HighSchoolData.swift
@@ -0,0 +1,16 @@
+//
+//  HighSchoolData.swift
+//  NYCHighSchools
+//
+//  Created by Matthew Lintlop on 2/1/18.
+//  Copyright © 2018 Matthew Lintlop. All rights reserved.
+//
+
+import Foundation
+
+struct HighSchoolData {
+    var name: String
+    var satReadingScore: Float
+    var satWritingScore: Float
+    var satMathScore: Float
+}
---
+++ NYCHighSchools/Services.swift
@@ -0,0 +1,14 @@
+//
+//  Services.swift
+//  NYCHighSchools
+//
+//  Created by Matthew Lintlop on 2/1/18.
+//  Copyright © 2018 Matthew Lintlop. All rights reserved.
+//
+
+import Foundation
+
+class Services {
+    var nycHighScoolsData: [HighSchoolData] = []        // data for each high school in NYC
+    
+}
