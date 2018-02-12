291dd6e9e2a14947d9750de1a97734572d417e33
Matt Lintlop <matt_lintlop@icloud.com>
2018-02-11 17:31:32 -0800

    Added HighSchoolNameCell

---
+++ NYCHighSchools/HighSchoolNameCell.swift
@@ -0,0 +1,26 @@
+//
+//  HighSchoolNameCell.swift
+//  NYCHighSchools
+//
+//  Created by Matt Lintlop on 2/11/18.
+//  Copyright © 2018 Matthew Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class HighSchoolNameCell: UITableViewCell {
+
+    @IBOutlet weak var highSchoolName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
