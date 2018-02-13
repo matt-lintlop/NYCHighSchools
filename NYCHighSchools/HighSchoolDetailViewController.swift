c8eee518f0bcf8ba95cfdf9ebe8dabe8c0e80c4e
Matt Lintlop <matt_lintlop@icloud.com>
2018-02-13 11:21:59 -0800

    Added HighSchoolDetailViewController & SettingTableViewController to main.storyboard

---
+++ NYCHighSchools/HighSchoolDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HighSchoolDetailViewController.swift
+//  NYCHighSchools
+//
+//  Created by Matt Lintlop on 2/13/18.
+//  Copyright © 2018 Matthew Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class HighSchoolDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
