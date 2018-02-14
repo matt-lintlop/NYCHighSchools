939e6c71c7cb407156e6dff830c59e1103bb0f76
Matt Lintlop <matt_lintlop@icloud.com>
2018-02-14 08:10:35 -0800

    Added SortDataByTypeCell/

--- NYCHighSchools/SettingsTableViewController.swift
+++ NYCHighSchools/SettingsTableViewController.swift
@@ -32,24 +32,20 @@ class SettingsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 6
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SortDataByTypeCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ NYCHighSchools/SortDataByCell.swift
@@ -0,0 +1,26 @@
+//
+//  SortDataByTypeCell
+//  NYCHighSchools
+//
+//  Created by Matt Lintlop on 2/14/18.
+//  Copyright © 2018 Matthew Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class SortDataByTypeCell: UITableViewCell {
+    
+    @IBOutlet weak var sortTypeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
