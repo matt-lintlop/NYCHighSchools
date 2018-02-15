9a56997fd39061fd2399970a26f4d565042aede0
Matt Lintlop <matt_lintlop@icloud.com>
2018-02-14 16:16:39 -0800

    Removed section title from high school VC.

--- NYCHighSchools/CityHighSchoolsTableViewController.swift
+++ NYCHighSchools/CityHighSchoolsTableViewController.swift
@@ -73,15 +73,6 @@ class CityHighSchoolsTableViewController: UITableViewController {
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-           return "NYC City High Schools"
-        }
-        else {
-            return nil
-        }
-    }
-  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
     
